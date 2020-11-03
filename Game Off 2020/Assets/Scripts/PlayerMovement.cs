e9df727c2e1216ff0ee3a813d9f0199686ce92b7
OfCroma <67212858+OfCroma@users.noreply.github.com>
2020-11-03 23:47:56 +0100

    movementChanges

---
+++ Game Off 2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocita = 5f; 
+    public float jumpForce = 10f; 
+    private float moveInput; 
+    private Rigidbody2D rb; 
+    private bool facingRight = true; 
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadious;
+    public LayerMask whatIsGround;
+
+    private int extraJump;
+    public int extraJumpValue;
+
+    void Start()
+    {
+        extraJump = extraJumpValue;
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    void FixedUpdate() 
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadious, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal"); 
+        rb.velocity = new Vector2(moveInput * velocita, rb.velocity.y); 
+
+        if (facingRight == false && moveInput > 0) 
+        {
+            Flip(); 
+        }
+        else if (facingRight == true && moveInput < 0) 
+        {
+            Flip(); 
+        }
+    }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJump = extraJumpValue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && extraJump > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJump == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+
+        facingRight = !facingRight; 
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
