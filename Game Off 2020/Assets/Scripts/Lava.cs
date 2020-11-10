6e2bdfd531d9585c0f427d97448dc4bc823df775
LittleDonkey146 <LittleDonkey146@users.noreply.github.com>
2020-11-10 17:01:40 +0200

    alex

--- Game Off 2020/Assets/Scripts/Lava.cs
+++ Game Off 2020/Assets/Scripts/Lava.cs
@@ -6,7 +6,7 @@ public class Lava : MonoBehaviour
 
     public bool lavaActivation = false;
     public GameObject lavaPrefab; //The lava prefab from our "Prefabs" folder in unity
-    //public GameObject bigLavaPrefab;
+    public GameObject bigLavaPrefab;
 
     void Start()
     {
@@ -19,7 +19,7 @@ public class Lava : MonoBehaviour
         {
             lavaActivation = true;
             StartCoroutine(LavaInstantiation());
-            //StartCoroutine(BigLava());
+            StartCoroutine(BigLava());
         }
     }
 
@@ -31,17 +31,16 @@ public class Lava : MonoBehaviour
             yield return new WaitForSeconds(Random.Range(0f, 10f));
 
             GameObject lavaDrop = Instantiate(lavaPrefab, transform.position, Quaternion.AngleAxis(90f, Vector3.forward)) as GameObject; // Instantiated as GameObject, just to be ok if we need anything else
-            //StartCoroutine(BigLava());
         }
     }
 
-    /*public IEnumerator BigLava()
+    public IEnumerator BigLava()
     {
         while (lavaActivation == true)
         {
-            yield return new WaitForSeconds(Random.Range(0f, 10f));
+            yield return new WaitForSeconds(Random.Range(30f, 60f));
 
             GameObject bigLavaDrop = Instantiate(bigLavaPrefab, transform.position, Quaternion.AngleAxis(90f, Vector3.forward)) as GameObject;
         }
-    }*/
+    }
 }
