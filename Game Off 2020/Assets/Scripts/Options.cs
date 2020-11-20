a94aa5ec7904b8d277000ef803f199c0ad9e05ac
LittleDonkey146 <LittleDonkey146@users.noreply.github.com>
2020-11-20 15:21:27 +0200

    alex

--- Game Off 2020/Assets/Scripts/Options.cs
+++ Game Off 2020/Assets/Scripts/Options.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
-    public float slider;
+    public Slider slider;
+    public float sliderValue;
+
+    private void Start()
+    {
+
+    }
 
     public void SetVolume(float volume)
     {
-        slider = GetComponent<Slider>().value;
-        volume = slider;
+        
+        sliderValue = volume;
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
