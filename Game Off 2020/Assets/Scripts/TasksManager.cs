7d487f6d372a0e1b870757c09788241cdad50b04
OfCroma <67212858+OfCroma@users.noreply.github.com>
2020-11-20 12:08:31 +0100

    someChanges

---
+++ Game Off 2020/Assets/Scripts/TasksManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TasksManager : MonoBehaviour
+{
+    public int tasksDone = 0;
+    public string tasksDoneCounter;
+    public Text text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        tasksDone += 1;
+        tasksDoneCounter = tasksDone.ToString();
+        text.text = tasksDoneCounter;
+    }
+}
