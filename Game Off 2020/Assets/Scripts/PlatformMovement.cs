a3c61449d5866ae25ce09f96c0599c48659beb5b
OfCroma <67212858+OfCroma@users.noreply.github.com>
2020-11-25 23:20:45 +0100

    Elevators(works pretty good)

---
+++ Game Off 2020/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nextPos;
+    public float speed = 2f;
+
+    public Transform elevatorTransform;
+    public Transform pointToGoTransform;
+
+    void Start()
+    {
+        posA = elevatorTransform.localPosition;
+        posB = pointToGoTransform.localPosition;
+        nextPos = posB;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move() 
+    {
+        elevatorTransform.localPosition = Vector3.MoveTowards(elevatorTransform.localPosition, nextPos, speed * Time.deltaTime);
+
+        if (Vector3.Distance(elevatorTransform.localPosition, nextPos) <= 0.1) 
+        {
+            ChangeNextPos();
+        }
+    }
+
+    public void ChangeNextPos() 
+    {
+        if (nextPos != posA)
+        {
+            nextPos = posA;
+        }
+        else 
+        {
+            nextPos = posB;
+        }
+    }
+}
